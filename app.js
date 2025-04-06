7994485dbd3fcced547a21c9ca4daad2143bb1ca
Jaylen Guerrero <jaylenguerrero@u.boisestate.edu>
2025-04-06 08:52:37 -0600

    started create

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/src/pages/index/index.html');
 });
 
+app.get('/create', (req, res) => {
+    res.sendFile(__dirname + '/src/pages/create/create.html');
+});
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 })
